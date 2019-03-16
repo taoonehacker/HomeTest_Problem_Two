aacb38b26192976a620e5dd25ed183dd93fba2f6
TDream <858609606@qq.com>
2019-03-16 23:07:39 +0800

    Add project files.

---
+++ ThoughtWorks.HomeTest.Merchant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThoughtWorks.HomeTest.Merchant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
